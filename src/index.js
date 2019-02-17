c9fcb53043cf2a7902dd996e6f9cde83ccb61032
M.Homma <f.xero123@gmail.com>
2019-02-17 18:16:32 +0900

    アプリケーション初期実装

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import * as reducers from './reducers';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Storeの生成
+const store = createStore(
+    // 1つのReducerで完結することはほぼ無いので、最初からcombineReducerを使う実装にしておく
+    combineReducers(reducers),
+    // Redux Middlewareにredux-loggerを設定
+    applyMiddleware(logger)
+);
+
+ReactDOM.render(
+    // storeをAppコンポーネントに紐付け
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import * as reducers from './';
+
+// 何もしないReducer
+// 変数に入れておくことで、後ほどのメンテがしやすくなる？
+export const noop = (state = {}) => state;
